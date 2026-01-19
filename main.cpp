1c466ec23f28722c4fd849012419a89a0fb8fde7
Smiley98 <smileysofficialcinema@gmail.com>
2026-01-19 10:31:14 -0500

    Updated circle setter

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
+#include <iostream>
+#include <cmath>
+
 class Circle
 {
 public:
-	float GetRadius() { return radius;  }
-	void SetRadius(float r) { radius = r; }
+	float GetRadius() { return radius; }
+	void SetRadius(float r)
+	{
+		// Setters only make sense if they do some form of validation.
+		// In this case, we ensure that our circle's radius cannot be negative!
+		r = std::max(r, 0.0f);
+		radius = r;
+	}
 
 private:
 	float radius;
