1f16dfcc6ac14484ead76c77b1b230de93793486
Smiley98 <smileysofficialcinema@gmail.com>
2026-01-19 11:09:22 -0500

    Added pointer example

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include <cmath>
 
+void Example1();
+void Example2();
+
+int main()
+{
+	//Example1();
+	Example2();
+	return 0;
+}
+
 class Circle
 {
 public:
@@ -17,10 +27,24 @@ private:
 	float radius;
 };
 
-int main()
+void Example1()
 {
 	Circle circle;
 	//circle.radius = -1.0f; <-- compiler error because radius is private (inaccessible)
 	circle.SetRadius(-1.0f);
-	return 0;
+}
+
+void Example2()
+{
+	int* ptr = nullptr;
+	int a = 5;
+	int b = 10;
+
+	ptr = &a;
+	std::cout << "Pointer address at a: " << ptr << std::endl;
+	std::cout << "Pointer value at a: " << *ptr << std::endl;
+
+	ptr = &b;
+	std::cout << "Pointer address at b: " << ptr << std::endl;
+	std::cout << "Pointer value at b: " << *ptr << std::endl;
 }
