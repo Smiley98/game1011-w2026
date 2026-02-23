467d19717caf72e6b02973d01e24252268de364c
Smiley98 <smileysofficialcinema@gmail.com>
2026-02-23 10:30:52 -0500

    Made basis for polymorphism

--- main.cpp
+++ main.cpp
@@ -1,4 +1,37 @@
+#include <iostream>
+#include <string>
+
+struct Character
+{
+	std::string Greet()
+	{
+		return "Default greeting";
+	}
+};
+
+struct Player : public Character
+{
+	std::string Greet()
+	{
+		return "Hello there kind traveller :)";
+	}
+};
+
+struct Enemy : public Character
+{
+	std::string Greet()
+	{
+		return "You won't stand a chance!";
+	}
+};
+
 int main()
 {
+	Player player;
+	Enemy enemy;
+
+	std::cout << player.Greet() << std::endl;
+	std::cout << enemy.Greet() << std::endl;
+
 	return 0;
 }
