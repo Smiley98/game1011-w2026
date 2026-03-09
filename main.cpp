eed0f96cc4a51bef6c10a54d53dd5c9fb846630d
Smiley98 <smileysofficialcinema@gmail.com>
2026-03-09 10:13:22 -0400

    Made integer square function

--- main.cpp
+++ main.cpp
@@ -1,4 +1,12 @@
+int Square(int n)
+{
+	return n * n;
+}
+
+// Need to overload for floats because our input of 2.5f truncates to 2
 int main()
 {
+	int a = Square(2);
+	float b = Square(2.5f);
 	return 0;
 }
