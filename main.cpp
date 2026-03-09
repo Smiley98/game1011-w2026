edf38a34b2ec4a24cf907e1cfd781fb4b56818b2
Smiley98 <smileysofficialcinema@gmail.com>
2026-03-09 10:20:54 -0400

    Switched to templated square function, explained new syntax

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
-int Square(int n)
+//int Square(int n)
+//{
+//	return n * n;
+//}
+//
+//float Square(float n)
+//{
+//	return n * n;
+//}
+
+// "Dog" is the name of our generic type;
+// "The Square function outputs a generic number given a generic number input"
+// Just like regular variables, your generic variable can be named whatever you like
+// (Dog, in my case)!
+template<typename Dog>
+Dog Square(Dog n)
 {
 	return n * n;
 }
 
-// Need to overload for floats because our input of 2.5f truncates to 2
 int main()
 {
 	int a = Square(2);
