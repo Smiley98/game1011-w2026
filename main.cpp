e7778cf9b3e7ad57623c06dc7c0f6ad2e9949cae
Smiley98 <smileysofficialcinema@gmail.com>
2026-03-09 10:59:23 -0400

    Added vector of integers

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 template<typename T>
 T Square(T n);
@@ -13,12 +14,22 @@ void Print3Types(A a, B b, C c);
 void Example1();
 void Example2();
 void Example3();
+void Example4();
+
+class IntArray
+{
+public:
+
+private:
+
+};
 
 int main()
 {
 	//Example1();
 	//Example2();
-	Example3();
+	//Example3();
+	Example4();
 	return 0;
 }
 
@@ -42,6 +53,17 @@ void Example3()
 {
 	Print3Types(1, 69.420f, "Hello <generic> world!");
 	Print3Types("Hello <generic> world!", 69.420f, 1);
+}
+
+void Example4()
+{
+	// This is the C# equivalent of List<int>(); (Dynamic Array)
+	std::vector<int> numbers;
+	numbers.push_back(1);
+	numbers.push_back(2);
+	numbers.push_back(3);
+	numbers.push_back(4);
+	numbers.push_back(5);
 }
 
 template<typename T>
