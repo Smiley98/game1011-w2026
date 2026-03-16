c10dfe9a3fea23b4297d31f387cc3fc5419494b2
Smiley98 <smileysofficialcinema@gmail.com>
2026-03-16 10:57:28 -0400

    Changed to stl array for safety

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <array>
 
 int main()
 {
-	std::string names[4] =
+	std::array<std::string, 4> names =
 	{
 		"Connor", "Rochelle", "Eliott", "Dominic"
 	};
@@ -13,26 +14,26 @@ int main()
 		std::ofstream file;
 		file.open("./data/File.txt");
 
-		for (int i = 0; i < 4; i++)
+		for (int i = 0; i < names.size(); i++)
 			file << names[i] << std::endl;
 
 		file.close();
 	}
 
-	for (int i = 0; i < 4; i++)
+	for (int i = 0; i < names.size(); i++)
 		names[i].clear();
 
 	{
 		std::ifstream file;
 		file.open("./data/File.txt");
 		
-		for (int i = 0; i < 4; i++)
+		for (int i = 0; i < names.size(); i++)
 			file >> names[i];
 
 		file.close();
 	}
 
-	for (int i = 0; i < 4; i++)
+	for (int i = 0; i < names.size(); i++)
 		std::cout << names[i] << std::endl;
 
 	return 0;
