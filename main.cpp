cda3110af243cc8fa40e8ea071f8c7341cd04ec1
Smiley98 <smileysofficialcinema@gmail.com>
2026-03-16 12:08:20 -0400

    Added word-by-word vs line-by-line

--- main.cpp
+++ main.cpp
@@ -5,17 +5,15 @@
 #include <cassert>
 
 void Example1();
+void Example2();
 
 void SaveNames(std::string file, std::vector<std::string> names);
 void LoadNames(std::string file, std::vector<std::string>& names);
 
 int main()
 {
-	//for (int i = 0; i < names.size(); i++)
-	//	std::cout << names[i] << std::endl;
-	//
-	Example1();
-
+	//Example1();
+	Example2();
 	return 0;
 }
 
@@ -33,6 +31,46 @@ void Example1()
 
 	for (int i = 0; i < names.size(); i++)
 		std::cout << names[i] << std::endl;
+}
+
+void Example2()
+{
+	const std::string file_name = "./data/Murphy.txt";
+
+	{
+		std::ofstream file;
+		file.open(file_name);
+
+		file << "Jayne Murphy" << std::endl;
+		file << "47 Jones Circle" << std::endl;
+		file << "Almond, NC 28702";
+
+		file.close();
+	}
+	
+	{
+		std::ifstream file;
+		file.open(file_name);
+
+		// Read the file word-by-word until we've reached the end
+		std::vector<std::string> text;
+
+		while (!file.eof())
+		{
+			// Read file word-by-word
+			//std::string word;
+			//file >> word;
+			//std::getline(file, word, ' ');	// <-- Delimiter is ' '
+
+			// Read file line-by-line
+			std::string line;
+			std::getline(file, line);			// <-- Delimiter is '\n' by default
+			text.push_back(line);
+		}
+
+		for (int i = 0; i < text.size(); i++)
+			std::cout << text[i] << std::endl;
+	}
 }
 
 void SaveNames(std::string file_name, std::vector<std::string> names)
