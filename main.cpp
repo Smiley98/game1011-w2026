4eac60cfe02e0e15a7c47a9eacba541f86922b91
Smiley98 <smileysofficialcinema@gmail.com>
2026-03-16 10:40:29 -0400

    Added names to file

--- main.cpp
+++ main.cpp
@@ -5,6 +5,12 @@ int main()
 {
 	std::ofstream out_file;
 	out_file.open("./data/File.txt");
+
+	out_file << "Connor" << std::endl;
+	out_file << "Rochelle" << std::endl;
+	out_file << "Eliott" << std::endl;
+	out_file << "Dominic" << std::endl;
+
 	out_file.close();
 	return 0;
 }
