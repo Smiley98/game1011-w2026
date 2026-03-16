ada595682e9bb9e68940100bb5ea1001b61155ff
Smiley98 <smileysofficialcinema@gmail.com>
2026-03-16 11:40:44 -0400

    Added flexible functions to example 1

--- main.cpp
+++ main.cpp
@@ -1,40 +1,60 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <array>
+#include <vector>
+#include <cassert>
+
+void Example1();
+
+void SaveNames(std::string file, std::vector<std::string> names);
+void LoadNames(std::string file, std::vector<std::string>& names);
 
 int main()
 {
-	std::array<std::string, 4> names =
-	{
-		"Connor", "Rochelle", "Eliott", "Dominic"
-	};
+	//for (int i = 0; i < names.size(); i++)
+	//	std::cout << names[i] << std::endl;
+	//
+	Example1();
 
+	return 0;
+}
+
+void Example1()
+{
+	const std::string file_name = "./data/File.txt";
+	std::vector<std::string> names = { "Connor", "Rochelle", "Eliott", "Dominic" };
+
+	SaveNames(file_name, names);
+
+	for (int i = 0; i < names.size(); i++)
+		names[i].clear();
+
+	LoadNames(file_name, names);
+
+	for (int i = 0; i < names.size(); i++)
+		std::cout << names[i] << std::endl;
+}
+
+void SaveNames(std::string file_name, std::vector<std::string> names)
+{
 	{
 		std::ofstream file;
-		file.open("./data/File.txt");
+		file.open(file_name);
 
 		for (int i = 0; i < names.size(); i++)
 			file << names[i] << std::endl;
 
 		file.close();
 	}
+}
+
+void LoadNames(std::string file_name, std::vector<std::string>& names)
+{
+	std::ifstream file;
+	file.open(file_name);
 
 	for (int i = 0; i < names.size(); i++)
-		names[i].clear();
+		file >> names[i];
 
-	{
-		std::ifstream file;
-		file.open("./data/File.txt");
-		
-		for (int i = 0; i < names.size(); i++)
-			file >> names[i];
-
-		file.close();
-	}
-
-	for (int i = 0; i < names.size(); i++)
-		std::cout << names[i] << std::endl;
-
-	return 0;
+	file.close();
 }
