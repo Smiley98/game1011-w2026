eff9e865b8c360e8f25df927e0df7639c6ce1fda
Smiley98 <smileysofficialcinema@gmail.com>
2026-03-23 10:52:17 -0400

    Added stack overflow

--- main.cpp
+++ main.cpp
@@ -1,4 +1,13 @@
+#include <iostream>
+
+void StackOverflow()
+{
+	std::cout << "This is NOT a good idea" << std::endl;
+	StackOverflow();
+}
+
 int main()
 {
+	//StackOverflow(); // Call this... if you dare!
 	return 0;
 }
