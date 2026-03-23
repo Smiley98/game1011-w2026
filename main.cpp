bd60ce6359e79b7503f3045120433f01730f5bbb
Smiley98 <smileysofficialcinema@gmail.com>
2026-03-23 11:03:26 -0400

    Added frequency function

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 void StackOverflow()
 {
@@ -6,8 +7,41 @@ void StackOverflow()
 	StackOverflow();
 }
 
+void HopefullyNotAStackOverflow(int count)
+{
+	if (count > 0)
+	{
+		std::cout << "This is NOT a good idea" << std::endl;
+		HopefullyNotAStackOverflow(count - 1);
+	}
+}
+
+int Frequency(char c, std::string str, int pos)
+{
+	if (pos == str.length())
+		return 0;
+
+	if (str[pos] == c)
+		return 1 + Frequency(c, str, pos + 1);
+	else
+		return 0 + Frequency(c, str, pos + 1);
+}
+
 int main()
 {
-	//StackOverflow(); // Call this... if you dare!
+	// Call this... if you dare!
+	//StackOverflow(); 
+
+	// This is the recursive equivalent of a for-loop that counts from 0-9!
+	//HopefullyNotAStackOverflow(10);
+	
+	// Whether its variables or functions, they all take up stack space!
+	//HopefullyNotAStackOverflow(20000); <--- stack overflow
+	//char data[1024]; <-- fine cause its only 1 kilobyte
+	//char data[1024 * 1024]; <-- stack overflow because its 1 megabyte (our stack must be < 1 megabyte by default)!!!
+
+	std::string name = "Connor";
+	int count = Frequency('n', name, 0);
+
 	return 0;
 }
