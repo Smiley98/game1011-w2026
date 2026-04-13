da300ee90a91aae35a8539c81a76dc6ec2b5b7e2
Smiley98 <smileysofficialcinema@gmail.com>
2026-04-13 11:32:38 -0400

    Made circular buffer

--- main.cpp
+++ main.cpp
@@ -1,31 +1,50 @@
 struct Queue
 {
-
+	const int capacity = 8;
+	int values[8]{};
+	int front = 0;
+	int back = 0;
 };
 
-// Add to the front of the queue
+// Add to the back of the queue
 void QueuePush(Queue& queue, int value)
 {
-
+	queue.values[queue.back] = value;
+	++queue.back %= queue.capacity;
 }
 
 // Remove from the front of our queue
 void QueuePop(Queue& queue)
 {
-
+	++queue.front %= queue.capacity;
 }
 
-int& QueueFront(const Queue& queue)
+int& QueueFront(Queue& queue)
 {
-
+	return queue.values[queue.front];
 }
 
-int& QueueBack(const Queue& queue)
+int& QueueBack(Queue& queue)
 {
-
+	return queue.values[queue.back];
 }
 
 int main()
 {
+	Queue queue;
+
+	QueuePush(queue, 3);
+	QueuePush(queue, 6);
+	QueuePush(queue, 9);
+
+	int a = QueueFront(queue);
+	QueuePop(queue);
+
+	int b = QueueFront(queue);
+	QueuePop(queue);
+
+	int c = QueueFront(queue);
+	QueuePop(queue);
+
 	return 0;
 }
