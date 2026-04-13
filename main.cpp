3a1cf8adf7e9ccb51e36dce86e82fcdbe396866e
Smiley98 <smileysofficialcinema@gmail.com>
2026-04-13 11:00:11 -0400

    Added queue API

--- main.cpp
+++ main.cpp
@@ -1,3 +1,30 @@
+struct Queue
+{
+
+};
+
+// Add to the front of the queue
+void QueuePush(Queue& queue, int value)
+{
+
+}
+
+// Remove from the front of our queue
+void QueuePop(Queue& queue)
+{
+
+}
+
+int& QueueFront(const Queue& queue)
+{
+
+}
+
+int& QueueBack(const Queue& queue)
+{
+
+}
+
 int main()
 {
 	return 0;
